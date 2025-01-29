ac6b9700e538ab6cf6f371fce97c4ff8465bd94d
Karim Haddad <karim.haddad@hotmail.com>
2025-01-29 18:55:57 -0500

    Add project files.

---
+++ JsonParser.cs
@@ -0,0 +1,282 @@
+﻿
+
+using System;
+
+namespace MyJsonParser
+{
+    internal class JsonParser
+    {
+        internal static bool TryParse(string input, out object? jsonObject)
+        {
+            jsonObject = null;
+
+            if (string.IsNullOrWhiteSpace(input)) return false;
+
+            try
+            {
+                jsonObject = JsonParser.InternalParse(input, 0).JsonObject;
+                Console.WriteLine(jsonObject);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+
+            return true;
+        }
+
+        private static (object? JsonObject, int) InternalParse(string input, int position)
+        {
+            int i = position;
+
+            while (i < input.Length)
+            {
+                var indexCharacter = input[i];
+
+                switch (indexCharacter)
+                {
+                    case ' ' or ',' or '\n':
+                        i++;
+                        continue;
+                    case '{':
+                        var (result, inc) = ParseObject(input, i);
+                        return (result, i - position + inc);
+                    case '[':
+                        var (arrResult, arrInc) = ParseArray(input, i);
+                        return (arrResult, i - position + arrInc);
+
+                    default:                    
+                        var (valResult, valInc) = ParseValue(input, i);
+                        return (valResult, i - position + valInc);
+
+                }
+            }
+
+            throw new InvalidOperationException($"unknown character {input[i]}");
+        }
+
+        private static (Dictionary<string, object?>, int) ParseObject(string input, int position)
+        {
+            var result = new Dictionary<string, object?>();
+
+            int i = position + 1;
+            var commaFoundWithoutClosing = false;
+            while(i < input.Length)
+            {
+                var indexCharacter = input[i];
+
+                if(indexCharacter is ' ' or '\n') {
+                    i++;
+                    continue;
+                }
+
+                if(indexCharacter is ',')
+                {
+                    commaFoundWithoutClosing = true;
+                    i++;
+                    continue;
+                }
+
+                if (indexCharacter is '}' && !commaFoundWithoutClosing)
+                {
+                    i++;
+                    break;
+                }
+
+
+                if (indexCharacter is ']')
+                {
+                    i++;
+                    break;
+                }
+
+                if (indexCharacter is '}' && commaFoundWithoutClosing)
+                {
+                    throw new InvalidOperationException("syntax error. Comma found in a wrong position"); 
+                }
+
+                if (indexCharacter is '"')
+                {
+                    commaFoundWithoutClosing = false;
+                    var (keyVal, val, inc) = ParseObjectProperty(input, i);
+                    i += inc;
+                    result[keyVal] = val;
+                    continue;
+                }
+
+                throw new InvalidOperationException($"Character {input[i]}");
+            }
+
+            return (result, i - position);
+        }
+
+        private static (string key, object? objectVal, int increment) ParseObjectProperty(string input, int position)
+        {
+            int i = position;
+  
+            var (key, keyInc) = ParseString(input, i);
+
+            i += keyInc;
+
+            var indexCharacter = input[i];
+
+            while (indexCharacter != ':')
+            {
+                i++;
+                indexCharacter = input[i];
+            }
+
+            i++;
+
+            var (value, valueInc) = InternalParse(input, i);
+            i += valueInc;
+
+            return (key, value, i - position);
+            
+        }
+
+        private static (string keyVal, int increment) ParseString(string input, int position)
+        {
+            int i = position + 1;
+            string keyVal = "";
+
+            while (i < input.Length)
+            {
+                if (input[i] is '\"' or '"')
+                {
+                    i++;
+                    break;
+                }
+
+                keyVal += input[i++];
+            }
+
+            return (keyVal, i - position);
+        }
+
+        private static (object?[], int) ParseArray(string input, int position)
+        {
+            int i = position + 1;
+
+            var list = new List<object?>();
+
+            while(i < input.Length)
+            {
+                var indexedCharacter = input[i];
+
+                if (indexedCharacter is ']')
+                {
+                    i++;
+                    break;
+                }
+
+                if (indexedCharacter is ',' or ' ' or '\n')
+                {
+                    i++;
+                    continue;
+                }
+
+                var (valueObj, inc) = InternalParse(input, i);
+                list.Add(valueObj);
+                i += inc; 
+            }
+
+            return (list.ToArray(), i - position);
+        }
+
+        private static (object?, int) ParseValue(string input, int position)
+        {
+            int i = position;
+
+            while(i < input.Length)
+            {
+                var indexCharacter = input[i];
+
+                if (indexCharacter is ' ' or '\n' or '\t') {
+                    i++;
+                    continue;
+                }
+
+                if (indexCharacter == '"')
+                {
+                    var (str, inc) = ParseString(input, i);
+                    return (str, i - position + inc);
+                }
+
+                if (IsDigit(indexCharacter))
+                {
+                    var(num, inc) = ParseNumber(input, i);
+                    return (num, i - position + inc);
+                }
+
+                if(indexCharacter == 'n') {
+
+                    TryParseExactValue(input, i, "null");
+
+                    return (null, i - position + 4);
+                }
+
+                if (indexCharacter == 't')
+                {
+                    TryParseExactValue(input, i, "true");
+
+                    return (true, i - position + 4);
+                } 
+                
+                if (indexCharacter == 'f')
+                {
+                    TryParseExactValue(input, i, "false");
+
+                    return (false, i - position + 5);
+                }
+
+                Console.WriteLine(input.Substring(0, i));
+
+                throw new InvalidOperationException($"unknown character {indexCharacter}");
+            }
+
+            throw new InvalidOperationException($"unknown character {input[i]}");
+        }
+
+        private static (object? nullVal, int inc) TryParseExactValue(string input, int position, string exactValue)
+        {
+            int i = position;
+            var value = "";
+            while(i < input.Length)
+            {
+                if(input[i] is ',' or '}' or ']')
+                {
+                    break;
+                }
+
+                value += input[i++];
+            }
+
+            if (value.Trim() != exactValue)
+            {
+                throw new InvalidOperationException("Invalid Token");
+            }
+
+            return (null, i - position);
+        }
+
+        private static bool IsDigit(char indexCharacter)
+        {
+            return indexCharacter is '1' or '2' or '3' or '4' or '5' or '6' or '7' or '8' or '9' or '0';
+        }
+
+        private static (object?, int) ParseNumber(string input, int position)
+        {
+            int i = position;
+            string number = "";
+
+            while (i < input.Length && IsDigit(input[i]))
+            {
+                number += input[i++];
+            }
+
+            return (int.Parse(number), i - position);
+        }
+    }
+}
---
+++ JsonParserTests.cs
@@ -0,0 +1,193 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace MyJsonParser
+{
+    public class JsonParserTests
+    {
+        [Fact]
+        public void Brackets_Are_Valid_Json()
+        {
+            string filePath = @"TestSteps/Step1/valid.json";
+
+            string content = File.ReadAllText(filePath);
+
+            JsonParser.TryParse(content, out var result).Should().BeTrue();
+            result.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void Empty_String_Is_Invalid_Json()
+        {
+            string filePath = @"TestSteps/Step1/invalid.json";
+
+            string content = File.ReadAllText(filePath);
+
+            JsonParser.TryParse(content, out var result).Should().BeFalse();
+            result.Should().BeNull();
+        }
+
+        [Theory]
+        [InlineData("12 ", 12)]
+        [InlineData(" 34", 34)]
+        [InlineData(" 56 ", 56)]
+        public void ParseNumber(string valueType, int expected)
+        {
+            JsonParser.TryParse(valueType, out var result).Should().BeTrue();
+            result.As<int>().Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("{\"key\": test}")]
+        [InlineData("{\"key\": tree }")]
+        [InlineData("{\"key\": temp}")]
+        [InlineData("{\"key\": famin}")]
+        [InlineData("{\"key\": feast}")]
+        [InlineData("{\"key\": note}")]
+        [InlineData("{\"key\": nllu}")]
+        public void ParseInvalidTokens(string valueType)
+        {
+            JsonParser.TryParse(valueType, out var result).Should().BeFalse();
+        }
+
+        [Fact]
+        public void ParseNull()
+        {
+            JsonParser.TryParse("{\"key\": null}", out var result).Should().BeTrue();
+            result.As<Dictionary<string, object?>>()["key"].Should().BeNull();
+
+            JsonParser.TryParse("{\"key\": null }", out var res2).Should().BeTrue();
+            res2.As<Dictionary<string, object?>>()["key"].Should().BeNull();
+        }
+
+        [Theory]
+        [InlineData("{\"key\": true}", true)]
+        [InlineData("{\"key\": true }", true)]
+        [InlineData("{\"key\": false}", false)]
+        [InlineData("{\"key\": false }", false)]
+        public void ParseBoolean(string valueType, bool expected)
+        {
+            JsonParser.TryParse(valueType, out var result).Should().BeTrue();
+            result.As<Dictionary<string, object?>>()["key"].Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("[\"hello\", \"world\"]", new string[] { "hello", "world" })]
+        [InlineData("[\"hello\"]", new string[] { "hello"})]
+        [InlineData("[\"hello\", 123, null, true, false]", new object[] { "hello", 123, null, true, false })]
+        public void ParseArray(string array, object?[] expected)
+        {
+            JsonParser.TryParse(array, out var result).Should().BeTrue();
+            for (int i = 0; i < expected.Length; i++)
+            {
+                result.As<object[]>()[i].Should().Be(expected[i]);
+            }
+        }
+
+        [Theory]
+        [InlineData("TestSteps/Step2/valid.json", 1)]
+        [InlineData("TestSteps/Step2/valid2.json", 2)]
+        public void ParseValidJsonStep2(string filePath, int expectedCount)
+        {
+            string content = File.ReadAllText(filePath);
+
+            JsonParser.TryParse(content, out var result).Should().BeTrue();
+            result.Should().NotBeNull();
+
+            result.As<Dictionary<string, object?>>()["key"].Should().Be("value");
+
+            for (int i = 1; i < expectedCount; i++)
+            {
+                result.As<Dictionary<string, object?>>()[$"key{expectedCount}"].Should().Be("value");
+            }
+        }
+
+        [Theory]
+        [InlineData("TestSteps/Step2/invalid.json")]
+        [InlineData("TestSteps/Step2/invalid2.json")]
+        public void ParseInValidJsonStep2(string filePath)
+        {
+            string content = File.ReadAllText(filePath);
+
+            JsonParser.TryParse(content, out var result).Should().BeFalse();
+        }
+
+        [Fact]
+        public void ParseValidJsonStep3()
+        {
+            string content = File.ReadAllText(@"TestSteps/Step3/valid.json");
+
+            JsonParser.TryParse(content, out var result).Should().BeTrue();
+
+            result.As<Dictionary<string, object?>>()["key1"].Should().Be(true);
+            result.As<Dictionary<string, object?>>()["key2"].Should().Be(false);
+            result.As<Dictionary<string, object?>>()["key3"].Should().Be(null);
+            result.As<Dictionary<string, object?>>()["key4"].Should().Be("value");
+            result.As<Dictionary<string, object?>>()["key5"].Should().Be(101);         
+        }
+
+        [Fact]
+        public void ParseInValidJsonStep3()
+        {
+            string content = File.ReadAllText(@"TestSteps/Step3/invalid.json");
+
+            JsonParser.TryParse(content, out var result).Should().BeFalse();
+        }
+
+        [Fact]
+        public void ParseValid1JsonStep4()
+        {
+            string content = File.ReadAllText(@"TestSteps/Step4/valid.json");
+
+            JsonParser.TryParse(content, out var result).Should().BeTrue();
+
+            result.As<Dictionary<string, object?>>()["key"].Should().Be("value");
+            result.As<Dictionary<string, object?>>()["key-n"].Should().Be(101);
+            result.As<Dictionary<string, object?>>()["key-o"].Should().NotBeNull();
+            result.As<Dictionary<string, object?>>()["key-o"].Should().BeOfType<Dictionary<string, object?>?>();
+            result.As<Dictionary<string, object?>>()["key-l"].Should().BeOfType<object?[]>();
+        }
+        
+        [Fact]
+        public void ParseValid2JsonStep4()
+        {
+            string content = File.ReadAllText(@"TestSteps/Step4/valid2.json");
+
+            JsonParser.TryParse(content, out var result).Should().BeTrue();
+
+            result.As<Dictionary<string, object?>>()["key"].Should().Be("value");
+            result.As<Dictionary<string, object?>>()["key-n"].Should().Be(101);
+            result.As<Dictionary<string, object?>>()["key-o"].Should().NotBeNull();
+            result.As<Dictionary<string, object?>>()["key-o"].Should().BeOfType<Dictionary<string, object?>?>();
+            result.As<Dictionary<string, object?>>()["key-o"]?.As<Dictionary<string, object?>>()["inner key"].Should().BeOfType<string>();
+            result.As<Dictionary<string, object?>>()["key-o"]?.As<Dictionary<string, object?>>()["inner key"].Should().Be("inner value");
+
+            result.As<Dictionary<string, object?>>()["key-l"].Should().BeOfType<object?[]>();
+            result.As<Dictionary<string, object?>>()["key-l"].As<object?[]>()[0].Should().Be("list value");
+        }
+
+        [Fact]
+        public void ParseInValidJsonStep4()
+        {
+            string content = File.ReadAllText(@"TestSteps/Step4/invalid.json");
+
+            JsonParser.TryParse(content, out var result).Should().BeFalse();
+        }
+
+        [Theory]
+        [InlineData("TestSteps/Step5/valid.json")]
+        [InlineData("TestSteps/Step5/valid2.json")]
+        public void ParseValidJsonStep5(string filePath)
+        {
+            string content = File.ReadAllText(filePath);
+
+            JsonParser.TryParse(content, out var result).Should().BeTrue();
+            result.Should().NotBeNull();
+        }
+
+
+
+
+
+    }
+}
